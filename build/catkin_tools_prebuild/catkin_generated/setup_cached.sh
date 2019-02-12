133873e5dafaf8f29d66e55776d1b2e1de9e57e7
laurencejbelliott <laurencejbelliott@gmail.com>
2019-02-12 14:01:31 +0000

    Converted repo to catkin workspace

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/loz/robG13/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/loz/bin:/home/loz/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/morse-blender-2.65-py-3.3/bin:/opt/morse-blender-2.65-py-3.3/bin:/opt/morse-blender-2.65-py-3.3/bin"
+export PKG_CONFIG_PATH="/opt/morse-blender-2.65-py-3.3/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig:/opt/morse-blender-2.65-py-3.3/lib/pkgconfig:/opt/morse-blender-2.65-py-3.3/lib/pkgconfig"
+export PWD="/home/loz/robG13/build/catkin_tools_prebuild"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:/opt/morse-blender-2.65-py-3.3/lib/python3.3/dist-packages:/opt/morse-blender-2.65-py-3.3/lib/python3.3/dist-packages:/opt/morse-blender-2.65-py-3.3/lib/python3.3/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/loz/robG13/devel/.private/catkin_tools_prebuild/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/loz/robG13/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
---
+++ devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
@@ -0,0 +1,1 @@
+/home/loz/robG13/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
